2c03b9b434ffa757d199c7963b2e6deaed8ff7c4
Maaz Waheed <153950782+42Wor@users.noreply.github.com>
2024-09-07 21:20:05 -0700

    Add files via upload

---
+++ Assets/links.js
@@ -0,0 +1,38 @@
+
+// Placeholder functions for different sections.
+
+function project() {
+    window.location.href = 'Project/'; // Replace with your target URL
+}
+
+function portfolio() {
+    window.location.href = 'com/'; // Replace with your target URL
+}
+
+function Privacy() {
+    window.location.href = 'privacy'; // Replace with your target URL
+}
+
+function FAQs() {
+    window.location.href = 'com/'; // Replace with your target URL
+}
+
+function com() {
+       window. open ('com/')
+}
+
+function python() {
+    window.location.href = '../Project/python/';
+}
+
+function Neural() {
+    window.location.href = '../Project/Neural Networks(MNIST)/';
+}
+
+function xyz() {
+    window.location.href = '../Project/3D Scatter Plot Generator/';
+}
+
+function o() {
+    window.location.href = 'https://maazwaheed.netlify.app/Project/pytnho/';
+}
